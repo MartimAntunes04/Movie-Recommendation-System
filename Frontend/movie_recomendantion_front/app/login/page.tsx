2a91ff86dcf2f9989e7232d50e674f6b9176a640
Befelixp <be.felix09@gmail.com>
2025-10-26 15:41:51 +0000

    Register feito

--- Frontend/movie_recomendantion_front/app/login/page.tsx
+++ Frontend/movie_recomendantion_front/app/login/page.tsx
@@ -52,7 +52,7 @@ export default function Login() {
             </Button>
           </form>
           <p className="text-center text-sm text-slate-600 dark:text-slate-400">
-              Não tem uma conta?{" "}
+              Não tem uma conta? {" "}
               <Button variant="link" className="text-blue-600 hover:cursor-pointer hover:text-blue-700 p-0 h-auto font-medium">
                 <Link href="/register">Cadastre-se</Link>
               </Button>
